9ac4a9c2f4bbebc6b1908d4482b567ba82d08e60
Arnaldo Morais <arma@cin.ufpe.br>
2019-04-06 00:31:46 -0300

    Add coments to the codes

--- base_functions/ideal_lp.m
+++ base_functions/ideal_lp.m
@@ -1,4 +1,11 @@
 function hd = ideal_lp(wc,M)
+% Ideal LowPass filter computation
+% --------------------------------
+% [hd] = ideal_lp(wc,M)
+% hd = ideal impulse response between 0 to M-1
+% wc = cutoff frequency in radians
+% M = length of the ideal filter
+%
 alpha = (M-1)/2
 n = [0:(M-1)];
 m = n - alpha + eps;
--- base_functions/lista4_q2.m
+++ base_functions/lista4_q2.m
@@ -3,8 +3,10 @@ wc1 = 0.3*pi;
 wc2 = 0.4*pi;
 wc3 = 0.5*pi;
 wc4 = 0.6*pi;
+%tr_width = wc2 - wc1;
 As = 50;
 Rp = 0.5;
+%M = ceil(6.6*pi/tr_width);
 M = 120;
 hd = ideal_lp(pi,M)+ideal_lp(wc3,M) - ideal_lp(wc4,M);
 hd = hd + ideal_lp(wc1,M) - ideal_lp(wc2,M);
@@ -23,4 +25,4 @@ axis([0 M-1 0 1.1]); xlabel('n'); ylabel('w[n]');
 subplot(2,2,3); stem(n,h); title('Resposta ao Impulso Atual');
 axis([0 M-1 -0.4 0.5]); xlabel('n'); ylabel('h[n]');
 subplot(2,2,4); plot(w/pi,db); title('Magnitude em dB'); grid
-axis([0 1 -150 10]); xlabel('Frequenci em pi unidades'); ylabel('Decibeis');
+axis([0 1 -150 10]); xlabel('Frequencia em pi unidades'); ylabel('Decibeis');
